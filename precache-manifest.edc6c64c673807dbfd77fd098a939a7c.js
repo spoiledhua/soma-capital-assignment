cb990c88a2869466259da3cc54b99d5205c1a57e
Victor Hua <vhua@princeton.edu>
2020-07-27 02:24:05 -0400

    Updates

---
+++ precache-manifest.edc6c64c673807dbfd77fd098a939a7c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bdc1e2b574fdbd277f1d331f2bc73e8",
+    "url": "/soma-capital-assignment/index.html"
+  },
+  {
+    "revision": "6e281ea6ffe042bd4a17",
+    "url": "/soma-capital-assignment/static/css/main.2c535127.chunk.css"
+  },
+  {
+    "revision": "dd840f6ee3717f7aaad7",
+    "url": "/soma-capital-assignment/static/js/2.e1a15485.chunk.js"
+  },
+  {
+    "revision": "6e281ea6ffe042bd4a17",
+    "url": "/soma-capital-assignment/static/js/main.8b154219.chunk.js"
+  },
+  {
+    "revision": "77e3c7d9377dd80eaa45",
+    "url": "/soma-capital-assignment/static/js/runtime-main.d1938404.js"
+  },
+  {
+    "revision": "1ef66f518225e74252d5ec9820dbb427",
+    "url": "/soma-capital-assignment/static/media/falcon1.1ef66f51.jpg"
+  },
+  {
+    "revision": "c68ad52cc997a7e59c344c8ad4876ae5",
+    "url": "/soma-capital-assignment/static/media/falcon9.c68ad52c.jpg"
+  },
+  {
+    "revision": "efb5ed002041e40cce46648eb96eed02",
+    "url": "/soma-capital-assignment/static/media/falconheavy.efb5ed00.jpg"
+  },
+  {
+    "revision": "47edea8e2681697900916b66e9d7b741",
+    "url": "/soma-capital-assignment/static/media/starship.47edea8e.jpg"
+  }
+]);
